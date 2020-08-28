7052ad4360bbf7201b1b74d6ce3e432ec3003027
Snouzy <mathiasnouzy@gmail.com>
2020-08-28 14:58:32 +0200

    typage redux thunk

--- src/actions/quiz.ts
+++ src/actions/quiz.ts
@@ -1,6 +1,8 @@
 import axios from "axios"
 import { IQuizList } from "../models";
 import { TYPES } from "./actions-types";
+import { AnyAction } from "redux";
+import { ThunkDispatch } from "redux-thunk";
 
 enum Difficulty {
     easy,
@@ -8,10 +10,9 @@ enum Difficulty {
     hard
 }
 export const getQuizListItem = (questionAmount: number, difficulty: Difficulty) => {
-    return async(dispatch: any) => {
+    return async(dispatch: ThunkDispatch<{},{},AnyAction>) => {
         const r = await axios.get<IQuizList>(`https://opentdb.com/api.php?amount=${questionAmount}&difficulty=${difficulty}`);
         dispatch({type: TYPES.getQuizListItems, payload: r.data.results});
     }
 }
-
 getQuizListItem(1, Difficulty.easy);
